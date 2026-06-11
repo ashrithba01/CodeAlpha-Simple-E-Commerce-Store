73a856bbe62315b72699218580adbfbad3049f2a
ashrithba01 <ashrithba01@gmail.com>
2026-06-11 23:14:07 +0530

    Ultimate UI upgrade with Hero banner, Toast notifications, and Glassmorphism

--- backend/settings.py
+++ backend/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',  # <--- ADD THIS EXACTLY HERE
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'store', # <-- Add this!
+    'store', 
 ]
 
 MIDDLEWARE = [
